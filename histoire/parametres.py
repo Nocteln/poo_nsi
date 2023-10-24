19f713c1ace31730da21625ba628da5eaf60dc59
Nocteln <eliottmieze@gmail.com>
2023-10-24 22:38:24 +0200

    ajout de commentaires

--- histoire/parametres.py
+++ histoire/parametres.py
@@ -1,51 +1,43 @@
 import tkinter as tk
 from tkinter import simpledialog
-
-# Fonction pour modifier les points de vie
 def parametres(default_joueur):
+    print("Veuillez vous rendre dans la fenetre qui vient de s'ouvrir pour modifier les parametres")
     def modifier_vie():
         nouveau_vie = simpledialog.askinteger("Modifier les points de vie", "Nouveau nombre de points de vie :")
         if nouveau_vie is not None:
             default_joueur[0] = int(nouveau_vie)
+        print("Nouveau nombre de points de vie :", default_joueur[0])  # Ajout d'un print de test
 
     def modifier_viande():
-        nouveau_viande = simpledialog.askinteger("Modifier la viande de départ",
-                                                 "Nouveau nombre de points de viande :")
+        nouveau_viande = simpledialog.askinteger("Modifier la viande de départ", "Nouveau nombre de points de viande :")
         if nouveau_viande is not None:
             default_joueur[2] = int(nouveau_viande)
+        print("Nouveau nombre de points de viande :", default_joueur[2])  # Ajout d'un print de test
+
     def modifier_degats():
-        nouveau_degats = simpledialog.askinteger("Modifier les degats", "Nouveau nombre de dégats que le joueur peut mettre :")
+        nouveau_degats = simpledialog.askinteger("Modifier les dégâts du joueur", "Nouveau nombre de dégâts du joueur :")
         if nouveau_degats is not None:
             default_joueur[3] = int(nouveau_degats)
+        print("Nouveaux dégâts du joueur :", default_joueur[3])  # Ajout d'un print de test
 
-
-    def fermer():
+    def fermer_fenetre():
         fenetre.destroy()
+
     # Code pour créer l'interface
     fenetre = tk.Tk()
     fenetre.title("Paramètres du jeu")
 
     bouton_modifier_vie = tk.Button(fenetre, text="Modifier les points de vie", command=modifier_vie)
     bouton_modifier_vie.pack()
-    bouton_modifier_viande = tk.Button(fenetre, text="Modifier la viande que l'on possède au départ",command=modifier_viande)
+
+    bouton_modifier_viande = tk.Button(fenetre, text="Modifier la viande de départ", command=modifier_viande)
     bouton_modifier_viande.pack()
+
     bouton_modifier_degats = tk.Button(fenetre, text="Modifier les dégâts du joueur", command=modifier_degats)
     bouton_modifier_degats.pack()
 
-    bouton_valider = tk.Button(fenetre, text="Valider", command=fermer)
+    bouton_valider = tk.Button(fenetre, text="Valider", command=fermer_fenetre)
     bouton_valider.pack(side="bottom")
 
     fenetre.mainloop()
     return default_joueur
-
-"""parametre à ajouter : 
-vie_mob_min/max
-degats_mob_min/max
-potion pour le sorcier
-
-
-+ avancé mais a faire :
-ajouter un joueur
-
-
-"""
--- main.py
+++ main.py
@@ -2,23 +2,25 @@ import perso
 import random
 import histoire
 from pvp import pvp
+from colorama import Fore, Back, Style, init
+init(autoreset=True)
 
 
-# definir le nombre de dégats que les mobs mettent
+# definir les parametres des mobs
 degats_mob_min = 0
 degats_mob_max = 3
 vie_mob_min = 5
 vie_mob_max =  25
 nb_degats_mob = random.randint(degats_mob_min, degats_mob_max)
 
-nb_degats_joueur = random.randint(degats_mob_min+1, degats_mob_max+1) #definir le nombre de dégats que le joueur met
-
-default_vie = 10
+# definir les parametres des joueurs
+nb_degats_joueur = random.randint(degats_mob_min+1, degats_mob_max+1)
+default_vie = 15
 default_nb_viande = 3
 default_nb_potion = 3
 default_joueur = [default_vie, default_nb_potion, default_nb_viande, nb_degats_joueur]
 
-
+# Instanciation des Mobs
 troll = perso.Troll( random.randint(vie_mob_min, vie_mob_max),  nb_degats_mob+2)
 vampire = perso.Vampire( random.randint(vie_mob_min, vie_mob_max),  nb_degats_mob)
 loupGarou = perso.LoupGarou( random.randint(vie_mob_min, vie_mob_max),  nb_degats_mob+1)
@@ -26,39 +28,40 @@ mob = [troll, vampire, loupGarou]
 
 choix_debut = ""
 
-while choix_debut != "1":
-    print("\n----La Quête des Légendes | Menu ----")
-    print("1. Lancer le jeu en solo\n2. Paramètres du jeu\n3. Joueur contres joueurs")
-    choix_debut = input("Saisissez votre choix : ")
+while choix_debut != "1": # Tant que la partie n'est pas lancée :
+    print("\n----" + Fore.RED +"La Quête des Légendes" + Fore.RESET + "|" + Fore.GREEN + "Menu" + Fore.RESET +"----")
+    print("1." +  Style.BRIGHT + " Lancer le jeu en solo" + Style.RESET_ALL +"\n2." +  Style.BRIGHT + " Paramètres du jeu" + Style.RESET_ALL +"\n3." +  Style.BRIGHT + " Joueur contres joueurs")
+    choix_debut = input("Saisissez votre choix : ") # Recupère le choix de l'utilisateur sur le lancement de la partie
 
     if choix_debut == "2":
-        # Fonction pour modifier les points de vie
+        # Fonction pour modifier certains parametres du jeu
         default_joueur = histoire.parametres(default_joueur)
     elif choix_debut == "3":
-        pvp()
+        pvp() # Lance un mode pvp
 
 
-print("\n\nBonjours et bienvenue dans La Quête des Légendes!\nPrépare toi à entrer dans un monde mysterieux plein de mystère\nDurant ton aventure, tu devra affronter de multiples danger et faire fasse à tout types d'épreuves! \nTient toi prêt, cela va commencer")
+print("\n\nBonjours et bienvenue dans" + Fore.RED +"La Quête des Légendes" + Fore.RESET + "\nPrépare toi à entrer dans un monde mysterieux plein de mystère\nDurant ton aventure, tu devra affronter de multiples danger et faire fasse à tout types d'épreuves! \nTient toi prêt, cela va commencer")
 
 print("Commencons par choisir votre Joueur !\n")
 
-joueur = histoire.select_perso(default_joueur)
+joueur = histoire.select_perso(default_joueur) # Selection du personnage
 
-
+# application des parametres modifié
 joueur.vie = default_joueur[0]
 joueur.nb_viande = default_joueur[2]
 if isinstance(joueur, perso.Sorcier):
     joueur.nb_potion = default_joueur[1]
 print(joueur.vie)
 
-print("\r\r")
-while joueur.vie > 0:
-    # Tour du joueur
-    print(f"{joueur.nom}, que souhaitez-vous faire?")
-    print("1. Attaquer")
-    print("2. Manger (si vous avez de la viande)")
-    if isinstance(joueur, perso.Sorcier):
-        print("3. Se soigner (pour les sorciers)")
+print("\n\n")
+# lancement de la boucle qui va finir lorseque le joueur n'aura plus de vie
+while True:
+    # Choix du joueur
+    print(Fore.CYAN + f"{joueur.nom}" + Fore.RESET + ", que souhaitez-vous faire?")
+    print("1." + Style.BRIGHT + " Attaquer")
+    print("2." + Style.BRIGHT + " Manger (si vous avez de la viande)")
+    if isinstance(joueur, perso.Sorcier): # si le personnage choisi est un sorcier, on lui propose aussi de se soigner
+        print("3." + Style.BRIGHT + " Se soigner")
 
     if isinstance(joueur, perso.Sorcier):
         choix = input("Saisissez 1, 2 ou 3 : ")
@@ -66,10 +69,10 @@ while joueur.vie > 0:
         choix = input("Saisissez 1 ou 2 : ")
     print("\n\n")
 
-    if choix == "1":
+    if choix == "1": # == attaquer
+        # appel du module histoire pour attaquer
        mob = histoire.attaque(joueur, mob, default_joueur, nb_degats_mob)
-    elif choix == "2" and joueur.nb_viande > 0:
-        joueur.nb_viande -= 1
+    elif choix == "2" and joueur.nb_viande > 0: # == manger
         joueur.manger()
         print(f"Vous mangez de la viande et avez maintenant {joueur.vie} points de vie.\nIl vous reste {joueur.nb_viande} viande")
     elif choix == "3" and isinstance(joueur, perso.Sorcier):
@@ -83,9 +86,9 @@ while joueur.vie > 0:
         break  # La boucle s'arrête si le joueur perd
     elif len(mob)==0:
         print("Vous avez gagné")
-        break
+        break # la boucle s'arrête si le joueur gagne
 
-    if choix != "2":
+    if choix != "2": # si l'on n'est pas en train de manger
         joueur.faim -= 0.5
         if (joueur.faim <= 4 and joueur.faim >=2):
             print(f"Attention, vous commencez à avoir faim ({joueur.faim}/10)")
